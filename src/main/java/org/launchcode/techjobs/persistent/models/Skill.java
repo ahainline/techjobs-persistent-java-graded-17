73039d92f62b1b1ba41029cf2c767955d5ca46cb
Ashley Hainline <aetindle@gmail.com>
2023-11-30 15:17:01 -0600

    add code for Controllers portion of Task 2 and uncomment associated tests

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -12,4 +12,8 @@ public class Skill extends AbstractEntity {
     public String getDescription() {
         return description;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
