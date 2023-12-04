ceb40f29be5f6d4899e0f7a40856b4af06a67230
Ashley Hainline <aetindle@gmail.com>
2023-12-04 09:33:44 -0600

    Add updated extra SQL query to pass test

--- queries.sql
+++ queries.sql
@@ -16,3 +16,4 @@
 --Part 4
 
     SELECT DISTINCT * FROM skill INNER JOIN job_skills ON skill.id = job_skills.skills_id ORDER BY name;
+    SELECT * FROM skill INNER JOIN job_skills ON skill.id = job_skills.skills_id ORDER BY name ASC;
